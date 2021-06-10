b0a6957ec101e3887160a64d31acad21013f3563
Abdify <abdullah.fahimm08@gmail.com>
2021-06-10 12:16:10 +0600

    organized

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
-
 const app = express();
-
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://boipoka:ZneV3qMTVin6RXF@cluster0.ykse1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
